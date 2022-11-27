f088d14c7b7f78f1f33922b99e0bf02756cacb64
MariaHRCastro <mariahribeiro@protonmail.com>
2022-11-26 22:11:38 -0300

    Relação oneToMany e manyToOne

--- src/main/java/com/educando/SpringBoot/resources/UserResource.java
+++ src/main/java/com/educando/SpringBoot/resources/UserResource.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.service.annotation.PatchExchange;
-
 import com.educando.SpringBoot.entities.User;
 import com.educando.SpringBoot.services.UserService;
 
