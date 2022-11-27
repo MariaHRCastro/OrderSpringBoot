cd2273df068f06206d1c42d9bb040fa9d2d118b5
MariaHRCastro <mariahribeiro@protonmail.com>
2022-11-27 19:56:29 -0300

    Categories

---
+++ src/main/java/com/educando/SpringBoot/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.educando.SpringBoot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educando.SpringBoot.entities.Category;
+import com.educando.SpringBoot.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj =  repository.findById(id);
+		return obj.get();
+	}
+}
